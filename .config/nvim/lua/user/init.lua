1422ba95a8b9cfe3425907fe38967e3c197e3da8
Maxime Brunet <max@brnt.mx>
2022-06-15 16:07:43 -0700

    nvim: Enable hadolint

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -34,6 +34,7 @@ local config = {
       config.sources = {
         null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.diagnostics.golangci_lint,
+        null_ls.builtins.diagnostics.hadolint,
         null_ls.builtins.diagnostics.shellcheck,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.gofumpt.with({
