6b00c9e1091d7dfaf3455188b2e4763e405c0dcd
Maxime Brunet <max@brnt.mx>
2022-06-24 11:13:32 -0700

    Fix formatting

--- configuration.nix
+++ configuration.nix
@@ -43,13 +43,13 @@
   environment.etc = {
     # https://nixos.wiki/wiki/PipeWire#Bluetooth_Configuration
     "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-      		bluez_monitor.properties = {
-      			["bluez5.enable-sbc-xq"] = true,
-      			["bluez5.enable-msbc"] = true,
-      			["bluez5.enable-hw-volume"] = true,
-      			["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-      		}
-      	'';
+      bluez_monitor.properties = {
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+      }
+    '';
   };
 
   environment.loginShellInit = ''
