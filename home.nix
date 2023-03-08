4d59f98a8a0f7a47d5b2fd419bf757d8da5a8269
Maxime Brunet <max@brnt.mx>
2023-03-07 17:30:48 -0800

    Update system.stateVersion to 22.11

--- home.nix
+++ home.nix
@@ -94,5 +94,5 @@ in
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "22.05";
+  home.stateVersion = "22.11";
 }
