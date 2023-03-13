30a3a61ae1db0359c0b60ee40c51bbebc04c0b1a
Maxime Brunet <max@brnt.mx>
2023-03-12 19:21:58 -0700

    nix: Create XDG user directories

--- home.nix
+++ home.nix
@@ -84,6 +84,11 @@ in
     };
   };
 
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
