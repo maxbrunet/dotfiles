a75a4bc68408b2e64244df764acd2665b0485383
Maxime Brunet <max@brnt.mx>
2023-11-11 12:23:07 -0800

    Update system

--- flake.nix
+++ flake.nix
@@ -7,17 +7,17 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-devpod.url = "github:maxbrunet/nixpkgs/feat/devpod";
+    nixpkgs-devpod.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
-    disko.url = "github:nix-community/disko/v1.0.0";
+    disko.url = "github:nix-community/disko/v1.1.0";
     disko.inputs.nixpkgs.follows = "nixos";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixos";
 
     asdf-kubectl = { url = "github:asdf-community/asdf-kubectl"; flake = false; };
     asdf-kustomize = { url = "github:Banno/asdf-kustomize"; flake = false; };
-    astronvim = { url = "github:AstroNvim/AstroNvim/v3.37.9"; flake = false; };
+    astronvim = { url = "github:AstroNvim/AstroNvim/v3.37.12"; flake = false; };
     base16-alacritty = { url = "github:aarowill/base16-alacritty"; flake = false; };
     base16-fzf = { url = "github:tinted-theming/base16-fzf"; flake = false; };
     base16-shell = { url = "github:tinted-theming/base16-shell"; flake = false; };
