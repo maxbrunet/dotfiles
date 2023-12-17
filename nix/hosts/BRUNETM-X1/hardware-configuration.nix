a0e4eec676c33d0e1fa3480adaf67fcd1541b32d
Maxime Brunet <max@brnt.mx>
2023-12-16 23:02:48 -0800

    Re-generate X1 hardware configuration

--- nix/hosts/BRUNETM-X1/hardware-configuration.nix
+++ nix/hosts/BRUNETM-X1/hardware-configuration.nix
@@ -39,6 +39,5 @@
   # networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
