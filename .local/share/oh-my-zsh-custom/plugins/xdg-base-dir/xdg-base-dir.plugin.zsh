09d185759dce5fef57982a5142a0c042bc635e40
Maxime Brunet <max@brnt.mx>
2024-03-01 15:29:53 -0800

    zsh: move Docker config under XDG config directory

--- .local/share/oh-my-zsh-custom/plugins/xdg-base-dir/xdg-base-dir.plugin.zsh
+++ .local/share/oh-my-zsh-custom/plugins/xdg-base-dir/xdg-base-dir.plugin.zsh
@@ -16,6 +16,7 @@ export TF_PLUGIN_CACHE_DIR="${CACHE_HOME}/terraform/plugins"
 # Config
 export AWS_CONFIG_FILE="${CONFIG_HOME}/aws/config"
 export AWS_SHARED_CREDENTIALS_FILE="${CONFIG_HOME}/aws/credentials"
+export DOCKER_CONFIG="${CONFIG_HOME}/docker"
 export OCI_CONFIG_FILE="${CONFIG_HOME}/oci/config"
 
 # Data
