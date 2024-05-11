fda451273351d2e380bc242910daa66c34df6742
Maxime Brunet <max@brnt.mx>
2024-05-11 10:23:14 -0700

    Update system

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,7 +22,7 @@ local sysname = vim.loop.os_uname().sysname
 require("lazy").setup({
   {
     "AstroNvim/AstroNvim",
-    version = "4.5.0",
+    version = "4.6.7",
     import = "astronvim.plugins",
   },
   {
