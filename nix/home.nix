89fc27b949803d45d4da860c757407cfad63bc5a
Maxime Brunet <max@brnt.mx>
2024-06-15 16:43:22 -0700

    Update stateVersions to 24.05

--- nix/home.nix
+++ nix/home.nix
@@ -196,5 +196,5 @@ in
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "23.05";
+  home.stateVersion = "24.05";
 }
