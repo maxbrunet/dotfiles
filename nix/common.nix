c7a433f6085620874c8c88f8e1c2f6cfee6197cc
Maxime Brunet <max@brnt.mx>
2025-01-26 10:47:33 -0300

    Update system

--- nix/common.nix
+++ nix/common.nix
@@ -164,19 +164,7 @@
     )
     regctl
     ripgrep
-    (
-      if stdenv.hostPlatform.isDarwin then
-        # https://github.com/NixOS/nixpkgs/pull/374697
-        unstable.ruff.overrideAttrs {
-          checkFlags = [ ];
-          cargoTestFlags = [
-            "--workspace"
-            "--exclude=red_knot"
-          ];
-        }
-      else
-        unstable.ruff
-    )
+    unstable.ruff
     rustup
     shellcheck
     shfmt
