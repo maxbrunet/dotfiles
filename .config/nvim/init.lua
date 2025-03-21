6936d09fc5f595fc42e7f02f708abfc8ee5d42e9
Maxime Brunet <max@brnt.mx>
2025-03-21 17:58:51 -0300

    Update system

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -24,7 +24,7 @@ local CO_API_KEY = os.getenv("CO_API_KEY")
 require("lazy").setup({
   {
     "AstroNvim/AstroNvim",
-    version = "4.32.1",
+    version = "4.32.3",
     import = "astronvim.plugins",
   },
   {
