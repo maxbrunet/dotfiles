84b8991f51d7ac92d9789c048f5dca78d64c44fb
Maxime Brunet <max@brnt.mx>
2025-08-17 11:44:49 -0300

    Update system

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -204,7 +204,7 @@ require("lazy").setup({
   },
   {
     "olimorris/codecompanion.nvim",
-    version = "17.13.0",
+    version = "17.15.0",
     enabled = CO_API_KEY ~= nil and CO_API_KEY ~= "",
     cmd = {
       "CodeCompanion",
