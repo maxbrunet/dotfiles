8cfd8715963eadd379077ff8cea5183f07fb9288
Maxime Brunet <max@brnt.mx>
2025-11-29 08:49:14 -0300

    Update stateVersion to 25.05

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -114,5 +114,5 @@ in
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 5;
+  system.stateVersion = 6;
 }
--- nix/home.nix
+++ nix/home.nix
@@ -209,5 +209,5 @@ in
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "24.11";
+  home.stateVersion = "25.05";
 }
